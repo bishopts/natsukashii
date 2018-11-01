c1463a3840de5a2a2f866f92343ea5d9c4727ba0
tyler seay <seay.tyler@gmail.com>
2018-11-01 12:49:30 -0400

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+const gulp 				= require('gulp');
+const sass 				= require('gulp-sass');
+const prefix 			= require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
+
+gulp.task('sass', function() {
+	return gulp.src('src/assets/sass/main.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(prefix({
+			browsers: ['last 2 versions'],
+			cascade: false
+		}))
+		.pipe(gulp.dest('src/assets/css'))
+		.pipe(browserSync.stream());
+});
+
+gulp.task('serve', gulp.series('sass', function() {
+	browserSync.init({
+		server: "./src"
+	});
+
+	gulp.watch('src/assets/sass/**').on('change', gulp.series('sass'));
+	gulp.watch('src/*.html').on('change', browserSync.reload);
+}));
+
+gulp.task('default', gulp.series('serve'));
