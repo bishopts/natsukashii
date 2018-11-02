d65f280c47afd91c48528eaddf0fd89d99732494
tyler seay <seay.tyler@gmail.com>
2018-11-02 19:30:21 -0400

    added first post

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const sass 				= require('gulp-sass');
 const prefix 			= require('gulp-autoprefixer');
 const browserSync = require('browser-sync').create();
 
+// Compile SASS files for the base site
 gulp.task('sass', function() {
 	return gulp.src('src/assets/sass/main.scss')
 		.pipe(sass().on('error', sass.logError))
@@ -14,6 +15,7 @@ gulp.task('sass', function() {
 		.pipe(browserSync.stream());
 });
 
+// Start Browser Sync, watch for changes in SASS, watch for changes in HTML
 gulp.task('serve', gulp.series('sass', function() {
 	browserSync.init({
 		server: "./src"
@@ -21,6 +23,7 @@ gulp.task('serve', gulp.series('sass', function() {
 
 	gulp.watch('src/assets/sass/**').on('change', gulp.series('sass'));
 	gulp.watch('src/*.html').on('change', browserSync.reload);
+	gulp.watch('src/posts/*.html').on('change', browserSync.reload);
 }));
 
 gulp.task('default', gulp.series('serve'));
