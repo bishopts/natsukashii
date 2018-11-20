ddfbb15e4032f0a9e2a5216147a5c8c8b405b400
tyler seay <seay.tyler@gmail.com>
2018-11-20 12:20:53 -0500

    added gulp build task

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp         = require('gulp');
 const sass         = require('gulp-sass');
 const prefix       = require('gulp-autoprefixer');
 const browserSync  = require('browser-sync').create();
+const imagemin     = require('gulp-imagemin');
 
 function css() {
   return gulp.src('src/assets/sass/main.scss')
@@ -20,7 +21,25 @@ function serve() {
   })
 };
 
+function copy() {
+  return gulp.src([
+    'src/**',
+    '!src/assets/post-assets/**',
+    '!src/assets/sass/**',
+    '!src/images/**',
+    '!src/post-template.html'
+  ])
+    .pipe(gulp.dest('dist/'));
+};
+
+function images() {
+  return gulp.src('src/images/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/images'));
+}
+
 exports.default = serve;
+exports.build = gulp.series(images, copy);
 
 gulp.watch('src/assets/sass/**', { events: 'all' }, css);
 gulp.watch('src/*.html', { events: 'all' }, browserSync.reload);
